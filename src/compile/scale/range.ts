bd4428292416582c33304cdac6f9c56a6e802555
Rob Miller <rob@omni.co>
2025-09-03 10:09:15 -0700

    another fix

--- src/compile/scale/range.ts
+++ src/compile/scale/range.ts
@@ -263,13 +263,16 @@ function defaultRange(channel: ScaleChannel, model: UnitModel): VgRange {
 
   const {domain, domainMid} = model.specifiedScales[channel];
 
+  const facetParent = getFacetModel(model);
+
   switch (channel) {
     case X:
     case Y: {
       // If there is no explicit width/height for discrete x/y scales
       if (util.contains(['point', 'band'], scaleType)) {
+        const sizeChannel = channel === X ? 'width' : 'height';
         const positionSize = getDiscretePositionSize(channel, size, config.view);
-        if (isStep(positionSize)) {
+        if (isStep(positionSize) && !facetParent?.hasExplicitSize(sizeChannel)) {
           const step = getPositionStep(positionSize, model, channel);
           return {step};
         }
